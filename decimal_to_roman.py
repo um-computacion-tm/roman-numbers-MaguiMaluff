14240252d144e3ec4ed5a1998b7fa565c282e29a
MaguiMaluff <m.maluff@alumno.um.edu.ar>
2023-03-22 11:32:57 -0300

    Mensaje

---
+++ decimal_to_roman.py
@@ -0,0 +1,116 @@
+import unittest
+
+def unidad_decimal(decimal):
+    unidadinicial = decimal % 10
+    unidad = ''
+    if unidadinicial <= 3:
+        unidad = 'I' * unidadinicial
+    elif unidadinicial == 4:
+        unidad = 'IV'
+    elif unidadinicial == 5:
+        unidad = 'V'
+    elif unidadinicial > 5 and unidadinicial < 9:
+        unidad = 'V'+('I' * (unidadinicial - 5))
+    elif unidadinicial == 9:
+        unidad = 'IX'
+    elif unidadinicial == 0:
+        unidad = '0'
+    return unidad
+
+def decimal_to_roman(decimal):
+    ## de 0 a 9
+    if decimal < 10:
+        unidadfinal = unidad_decimal(decimal)
+        return unidadfinal
+    ###del 10 al 39
+    elif decimal >= 10 and decimal < 40:
+        decimalinicial = decimal / 10
+        unidadfinal = unidad_decimal(decimal) 
+        return ("X" * int(decimalinicial)) + unidadfinal
+    ## del 40 al 49
+    elif decimal >= 40 and decimal < 50:
+        unidadfinal = unidad_decimal(decimal)
+        return "XL" + unidadfinal
+    ## del 50 al 89
+    elif decimal >= 50  and decimal < 90:
+        unidadfinal = unidad_decimal(decimal)
+        return "V" + (decimal - 50)
+
+## decimal decimal / 10 y unidad decimal % 10
+
+class TestDecimalToRoman(unittest.TestCase):
+    def test_uno(self):
+        # pre condicion
+        ### NO HAY ###
+        # proceso
+        resultado = decimal_to_roman(1)
+        # verificacion
+        self.assertEqual(resultado, 'I')
+
+    def test_dos(self):
+        resultado = decimal_to_roman(2)
+        self.assertEqual(resultado, 'II')
+
+    def test_tres(self):
+        resultado = decimal_to_roman(3)
+        self.assertEqual(resultado, 'III')
+
+    def test_diez(self):
+        resultado = decimal_to_roman(10)
+        self.assertEqual(resultado, 'X')
+
+    def test_cinco(self):
+        resultado = decimal_to_roman(5)
+        self.assertEqual(resultado, 'V')
+
+    def test_seis(self):
+        resultado = decimal_to_roman(6)
+        self.assertEqual(resultado, 'VI')
+
+    def test_siete(self):
+        resultado = decimal_to_roman(7)
+        self.assertEqual(resultado, 'VII')
+    
+    def test_ocho(self):
+        resultado = decimal_to_roman(8)
+        self.assertEqual (resultado, 'VIII')
+
+    def test_trescero(self):
+        resultado = decimal_to_roman(30)
+        self.assertEqual(resultado, 'XXX')
+
+    def test_veinte(self):
+        resultado = decimal_to_roman(20)
+        self.assertEqual(resultado, 'XX')
+
+    def test_unonueve(self):
+        resultado = decimal_to_roman(19)
+        self.assertEqual(resultado, 'XIX')
+    
+    def test_dostres(self):
+        resultado = decimal_to_roman(23)
+        self.assertEqual(resultado, 'XXIII')
+
+    def test_tresnueve(self):
+        resultado = decimal_to_roman(39)
+        self.assertEqual(resultado, 'XXXIX')
+    
+    def test_trescero(self):
+        resultado = decimal_to_roman(30)
+        self.assertEqual(resultado, 'XXX')
+
+    def test_cuarenta(self):
+        resultado = decimal_to_roman(40)
+        self.assertEqual(resultado, 'XL')
+    
+    def test_cuatrotres(self):
+        resultado = decimal_to_roman(43)
+        self.assertEqual(resultado, 'XLIII')
+    
+    def test_cuatronueve(self):
+        resultado = decimal_to_roman(49)
+        self.assertEqual(resultado, 'XLIX')
+
+
+if __name__ == '__main__':
+    unittest.main()
